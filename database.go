5c4ead7571397b10d201c6781b4b13cc10ff6745
sakeven <jc5930@sina.cn>
2015-06-24 18:28:12 +0800

    refactor

---
+++ database.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+    "gopkg.in/mgo.v2"
+    // "gopkg.in/mgo.v2/bson"
+    "log"
+    "os"
+)
+
+var DB *mgo.Database
+
+func MustConnectMongo() {
+
+}
+
+func ConnectMongo() error {
+    conn := ""
+    if len(os.Getenv("MONGODB_USERNAME")) > 0 {
+        conn += os.Getenv("MONGODB_USERNAME")
+
+        if len(os.Getenv("MONGODB_PASSWORD")) > 0 {
+            conn += ":" + os.Getenv("MONGODB_PASSWORD")
+        }
+
+        conn += "@"
+    }
+
+    if len(os.Getenv("MONGODB_PORT_27017_TCP_ADDR")) > 0 {
+        conn += os.Getenv("MONGODB_PORT_27017_TCP_ADDR")
+    } else {
+        conn += "localhost"
+    }
+
+    if len(os.Getenv("MONGODB_PORT_27017_TCP_PORT")) > 0 {
+        conn += ":" + os.Getenv("MONGODB_PORT_27017_TCP_PORT")
+    } else {
+        conn += ":27017"
+    }
+    // defaultly using "test" as the db instance
+    db := "test"
+
+    if len(os.Getenv("MONGODB_INSTANCE_NAME")) > 0 {
+        db = os.Getenv("MONGODB_INSTANCE_NAME")
+    }
+
+    conn += "/" + db
+
+    session, err := mgo.Dial(conn)
+    if err != nil {
+        return err
+    }
+
+    DB = session.DB(db)
+    PeopleC = session.DB(db).C("people")
+
+    return nil
+}
+
+func Drop() { // Drop Database
+    if IsDrop {
+        err := DB.DropDatabase()
+        if err != nil {
+            log.Println(err)
+        }
+    }
+}
--- main.go
+++ main.go
@@ -1,49 +1,50 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"os"
-	"strings"
-	"time"
+    "fmt"
+    "net/http"
+    "os"
+    "strings"
+    "time"
 )
 
 func main() {
-	http.HandleFunc("/", hello)
-	http.HandleFunc("/env", env)
+    MustConnectMongo()
+    http.HandleFunc("/", hello)
+    http.HandleFunc("/env", env)
 
-	fmt.Println("Start listening...")
-	go log()
-	err := http.ListenAndServe(":80", nil)
-	if err != nil {
-		panic(err)
-	}
+    fmt.Println("Start listening...")
+    go Log()
+    err := http.ListenAndServe(":80", nil)
+    if err != nil {
+        panic(err)
+    }
 }
 
-func log() {
-	for {
-		time.Sleep(30 * time.Second)
-		fmt.Println("Hello World !")
-	}
+func Log() {
+    for {
+        time.Sleep(30 * time.Second)
+        fmt.Println("Hello World !")
+    }
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
-    defer func(){
-        if e := recover(); e != nil{
+    defer func() {
+        if e := recover(); e != nil {
             fmt.Fprintln(res, e)
         }
     }()
-	fmt.Fprintln(res, "Hello World, ", GetResult())
+    fmt.Fprintln(res, "Hello World, ", GetResult())
 }
 
 func env(res http.ResponseWriter, req *http.Request) {
-	env := os.Environ()
+    env := os.Environ()
 
-	fmt.Fprintln(res, "List of Environtment variables : \n")
+    fmt.Fprintln(res, "List of Environtment variables : \n")
 
-	for index, value := range env {
-		name := strings.Split(value, "=") // split by = sign
+    for index, value := range env {
+        name := strings.Split(value, "=") // split by = sign
 
-		fmt.Fprintf(res, "[%d] %s : %v\n", index, name[0], name[1])
-	}
+        fmt.Fprintf(res, "[%d] %s : %v\n", index, name[0], name[1])
+    }
 }
